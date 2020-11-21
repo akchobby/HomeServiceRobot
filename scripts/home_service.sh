e086200f10f913c0c9bb9ad96f134675e9c30846
Anil Kumar Chavali <akchavali9@gmail.com>
2020-11-21 00:36:49 +0000

    scripts directories made common

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
  
 
-path_ws="/home/workspace/catkin_ws"
 
-xterm -e "cd ${path_ws}; source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE=${path_ws}/src/HomeServiceRobot/worlds/world/featured_world;roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e "cd $(rospack find add_markers) && cd ../; source ../../devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE=$(echo $(cd $(rospack find add_markers) && cd ../ && pwd))/worlds/world/featured_world;roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
 sleep 20
-xterm -e "cd ${path_ws}; source devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE=${path_ws}/src/HomeServiceRobot/map/maps/my_map.yaml;roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "cd $(rospack find add_markers) && cd ../; source ../../devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE=$(echo $(cd $(rospack find add_markers) && cd ../ && pwd))/map/maps/my_map.yaml;roslaunch turtlebot_gazebo amcl_demo.launch" &
 
 sleep 30
-xterm -e "cd ${path_ws}; source devel/setup.bash; roslaunch add_markers view_navigation.launch" &
+xterm -e "cd $(rospack find add_markers) && cd ../; source ../../devel/setup.bash; roslaunch add_markers view_navigation.launch" &
 
-sleep 90
-xterm -e "cd ${path_ws}; source devel/setup.bash; roslaunch pick_object pick_objects.launch" &
+sleep 30
+xterm -e "cd $(rospack find add_markers) && cd ../; source ../../devel/setup.bash; roslaunch pick_object pick_objects.launch" &
 
 sleep 5
-xterm -e "cd ${path_ws}; source devel/setup.bash; roslaunch add_markers add_markers.launch" &
+xterm -e "cd $(rospack find add_markers) && cd ../; source ../../devel/setup.bash; roslaunch add_markers add_markers.launch" &
 
 
 
--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
  
 
-path_ws="/home/workspace/catkin_ws"
-
-xterm -e "cd ${path_ws}; source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE=${path_ws}/src/HomeServiceRobot/worlds/world/featured_world;roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e "cd $(rospack find add_markers) && cd ../; source ../../devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE=$(echo $(cd $(rospack find add_markers) && cd ../ && pwd))/worlds/world/featured_world;roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
 sleep 20
-xterm -e "cd ${path_ws}; source devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE=${path_ws}/src/HomeServiceRobot/map/maps/my_map.yaml;roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "cd $(rospack find add_markers) && cd ../; source ../../devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE=$(echo $(cd $(rospack find add_markers) && cd ../ && pwd))/map/maps/my_map.yaml;roslaunch turtlebot_gazebo amcl_demo.launch" &
 
-sleep 15
-xterm -e "cd ${path_ws}; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-
+sleep 30
+xterm -e "cd $(rospack find add_markers) && cd ../; source ../../devel/setup.bash; roslaunch add_markers view_navigation.launch" &
